f5232961299d01c122ed76a485013fcd6939d04e
djosh <djoshandrew@gmail.com>
2021-06-03 11:03:09 -0700

    Added Collision for Tunnel, Everything except Automation for End is here

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import turtle
 import sys
 import math
 import random
+from inspect import v
 
 WIDTH = 1200
 HEIGHT = 800
@@ -22,6 +23,8 @@ wn.title("Pr")
 wn.setup(WIDTH, HEIGHT)
 wn.bgcolor(BLACK)
 wn.tracer(0)
+
+
 
 # Create pen
 pen = turtle.Turtle()
@@ -119,7 +122,7 @@ class End(Sprite):
 class Tunnel(Sprite):
     def __init__(self, x, y, width, height):
         Sprite.__init__(self, x, y, width, height)
-        self.color = GREEN
+        self.color = WHITE
 
 
 
@@ -130,8 +133,8 @@ class Tunnel(Sprite):
 # Create game objects
 end = End(-370, -180, 20,20)
 open = Opener(-350, -100, 80, 200)
-key = Key(-40, 20, 1, 1)
-tun = Tunnel(0, -5, 600, 20)
+key = Key(-40, 0, 1, 1)
+tun = Tunnel(0, -7, 600, 18)
 large = Large(180, 240, 30, 60)
 portal = Portal(400, -160, 20, 100)
 player = Player(0, 400, 20, 40)
@@ -190,12 +193,14 @@ while True:
             tun.y = 50
             key.width = 50
             key.height = 40
+            key.y = 20
         elif player.x > portal.x + portal.width / 2.0 and player.dx < 0:
             player.width = 10
             player.height = 20
             tun.y = 50
             key.width = 50
             key.height = 40
+            key.y = 20
         # Player is above
         elif player.y > portal.y:
             player.width = 10
@@ -203,6 +208,28 @@ while True:
             tun.y = 50
             key.width = 50
             key.height = 40
+            key.y = 20
+
+    # if player.is_aabb_collision(end):
+    #     if player.x < end.x - end.width / 2.0 and player.dx > 0:
+    #         wn.addshape('python2.gif')
+    #         pen.shape('python2.gif')
+    #         wn.mainloop()
+    #     # Player is to the right
+    #     elif player.x > end.x + end.width / 2.0 and player.dx < 0:
+    #         wn.addshape('python2.gif')
+    #         pen.shape('python2.gif')
+    #         wn.mainloop()
+    #     # Player is above
+    #     elif player.y > end.y:
+    #         wn.addshape('python2.gif')
+    #         pen.shape('python2.gif')
+    #         wn.mainloop()
+    #     # Player is below
+    #     elif player.y < end.y:
+    #         yes = wn.addshape('python2.gif')
+    #         pen.shape('python2.gif')
+    #         wn.mainloop()
 
 
     if player.is_aabb_collision(key):
@@ -226,6 +253,24 @@ while True:
             key.height = 0.0000001
             key.width = 0.0000001
             open.x = -300
+
+    if player.is_aabb_collision(tun):
+        # Player is to the left
+        if player.x < tun.x - tun.width / 2.0 and player.dx > 0:
+            player.dx = 0
+            player.x = tun.x - tun.width / 2.0 - player.width / 2.0
+        # Player is to the right
+        elif player.x > tun.x + tun.width / 2.0 and player.dx < 0:
+            player.dx = 0
+            player.x = tun.x + tun.width / 2.0 + player.width / 2.0
+        # Player is above
+        elif player.y > tun.y:
+            player.dy = 0
+            player.y = tun.y + tun.height / 2.0 + player.height / 2.0 - 1
+        # Player is below
+        elif player.y < tun.y:
+            player.dy = 0
+            player.y = tun.y - tun.height / 2.0 - player.height / 2.0
 
     if player.is_aabb_collision(open):
         if player.x < open.x - open.width / 2.0 and player.dx > 0:
@@ -273,6 +318,7 @@ while True:
     # Render (Draw stuff)
 
     # Render objects
+    # yes.render()
     tun.render()
     end.render()
     open.render()
